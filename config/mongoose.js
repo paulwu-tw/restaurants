810f4443a1492de349cd329363e35270fafe7532
paul.wu <tease0623@gmail.com>
2023-05-04 15:23:41 +0800

    project init

---
+++ config/mongoose.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+// chekce env
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const user = process.env.MONGODB_USER
+const password = process.env.MONGODB_PASSWORD
+const uri = process.env.MONGODB_URI
+
+// database connection
+mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "todo-list",
+    auth: {
+        username: user,
+        password: password
+    }
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb connect error')
+})
+
+db.once('open', () => {
+    console.log('mongodb connected...')
+})
+
+module.exports = db
