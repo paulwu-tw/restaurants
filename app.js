a9d07a0d80a21eb03efb126d8acc3af7e41849c6
paul.wu <tease0623@gmail.com>
2023-05-04 17:29:04 +0800

    feature: edit page ,and delete page

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ app.get('/', (req, res) => {
         .catch(err => console.log(err))
 })
 
+// search restaurants
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    restaurantsModel.find()
+        .lean()
+        .then(restaurantList => {
+            const restaurants = restaurantList.filter(restaurant =>
+                restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase())
+                || restaurant.category.toLowerCase().includes(keyword.trim().toLowerCase()))
+
+            if (restaurants.length) res.render('index', { restaurants, keyword })
+            else res.render('error', { keyword })
+        })
+})
+
 // new a restaurant
 // GET /restaurants/new
 app.get('/restaurants/new', (req, res) => {
@@ -42,7 +57,7 @@ app.post('/restaurants', (req, res) => {
 //GET /restaurants/:id
 app.get('/restaurants/:id', (req, res) => {
     const id = Number(req.params.id)
-    restaurantsModel.findOne({ id: id })
+    restaurantsModel.findOne({ id })
         .lean()
         .then(restaurant => res.render('show', { restaurant }))
         .catch(err => console.log(err))
@@ -51,10 +66,31 @@ app.get('/restaurants/:id', (req, res) => {
 
 // edit a restaurant info
 // GET /restaurants/:id/edit
+app.get('/restaurants/:id/edit', (req, res) => {
+    const info = req.body
+    const id = Number(req.params.id)
+    restaurantsModel.findOne({ id })
+        .lean()
+        .then(restaurant => res.render('edit', { restaurant }))
+        .catch(err => console.log(err))
+})
+
 // POST /restaurants/:id/edit
+app.post('/restaurants/:id/edit', (req, res) => {
+    const info = req.body
+    const id = Number(req.params.id)
+    restaurantsModel.updateOne({ id }, info)
+        .then(() => res.redirect(`/restaurants/${id}`))
+})
 
 // delete a restaurant
 // POST /restaurants/:id/delete
+app.post('/restaurants/:id/delete', (req, res) => {
+    const id = Number(req.params.id)
+    restaurantsModel.deleteOne({ id })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+})
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
