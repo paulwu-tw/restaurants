df20769559a01a9c87cc60d6d585558245156753
paul.wu <tease0623@gmail.com>
2023-05-04 16:30:46 +0800

    add show page

--- app.js
+++ app.js
@@ -11,12 +11,36 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
+
+// index page, brose restaurants
 app.get('/', (req, res) => {
     restaurantsModel.find()
         .lean()
         .then((restaurants) => res.render('index', { restaurants }))
         .catch(err => console.log(err))
 })
+
+// new a restaurant
+// GET /restaurants/new
+// POST /restaurants
+
+// show a restaurant detail
+//GET /restaurants/:id
+app.get('/restaurants/:id', (req, res) => {
+    const id = Number(req.params.id)
+    restaurantsModel.findOne({ id: id })
+        .lean()
+        .then(restaurant => res.render('show', { restaurant }))
+        .catch(err => console.log(err))
+
+})
+
+// edit a restaurant info
+// GET /restaurants/:id/edit
+// POST /restaurants/:id/edit
+
+// delete a restaurant
+// POST /restaurants/:id/delete
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
