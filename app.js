83f6621ca5e3cc6cbcb8df72b74501f542ef209d
paul.wu <tease0623@gmail.com>
2023-05-04 16:03:36 +0800

    add index page, query data from database

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const restaurantsModel = require('./models/restaurants')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
 
 app.engine('hbs', hbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('/')
+    restaurantsModel.find()
+        .lean()
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
