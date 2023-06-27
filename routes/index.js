be8c1a2b9cd59f32dad9877f91c71c95e7360cab
paul.wu <tease0623@gmail.com>
2023-06-27 20:48:19 +0800

    add user routes module

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const users = require('./modules/users')
 const restaurants = require('./modules/restaurants')
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
+router.use('/', home)
 
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// const User = require('../../models/user');
+
+// login page
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// login check
+router.post('/login', (req, res) => {
+    res.render('login');
+});
+
+// register page
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+// register check
+router.post('/register', (req, res) => {
+    res.render('register');
+});
+
+// logout
+router.get('/logout', (req, res) => {
+    res.render('logout');
+});
+
+module.exports = router;
